9424abedee5563241102df6b5f66aab77f91da2e
Hongyi Lyu <hongyi.lyu95@gmail.com>
2019-05-12 06:05:38 +0000

    cifar autoencoder

---
+++ cifar_autoencoder/autoencoder.py
@@ -0,0 +1,115 @@
+'''Trains a denoising autoencoder on MNIST dataset.
+Denoising is one of the classic applications of autoencoders.
+The denoising process removes unwanted noise that corrupted the
+true signal.
+Noise + Data ---> Denoising Autoencoder ---> Data
+Given a training dataset of corrupted data as input and
+true signal as output, a denoising autoencoder can recover the
+hidden structure to generate clean data.
+This example has modular design. The encoder, decoder and autoencoder
+are 3 models that share weights. For example, after training the
+autoencoder, the encoder can be used to  generate latent vectors
+of input data for low-dim visualization like PCA or TSNE.
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import keras
+from keras.layers import Activation, Dense, Input
+from keras.layers import Conv2D, Flatten
+from keras.layers import Reshape, Conv2DTranspose
+from keras.models import Model
+from keras import backend as K
+from keras.datasets import mnist
+import numpy as np
+from PIL import Image
+import pickle
+
+np.random.seed(1337)
+
+DATASET_DIR = '/home/bill.lv22/'
+
+x_adv_train, x_adv_test = pickle.load(open(DATASET_DIR+'fg.pkl'))
+x_train, x_test, y_train, y_test = pickle.load(open(DATASET_DIR+'cifar10.pkl'))
+
+# Network parameters
+image_size = x_train.shape[1]
+input_shape = (image_size, image_size, 3)
+batch_size = 128
+kernel_size = 3
+latent_dim = 16
+# Encoder/Decoder number of CNN layers and filters per layer
+layer_filters = [32, 64]
+
+# Build the Autoencoder Model
+# First build the Encoder Model
+inputs = Input(shape=input_shape, name='encoder_input')
+x = inputs
+# Stack of Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use MaxPooling2D as alternative to strides>1
+# - faster but not as good as strides>1
+for filters in layer_filters:
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=2,
+               activation='relu',
+               padding='same')(x)
+
+# Shape info needed to build Decoder Model
+shape = K.int_shape(x)
+
+# Generate the latent vector
+x = Flatten()(x)
+latent = Dense(latent_dim, name='latent_vector')(x)
+
+# Instantiate Encoder Model
+encoder = Model(inputs, latent, name='encoder')
+encoder.summary()
+
+# Build the Decoder Mod1l
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+# Stack of Transposed Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use UpSampling2D as alternative to strides>1
+# - faster but not as good as strides>1
+for filters in layer_filters[::-1]:
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=kernel_size,
+                        strides=2,
+                        activation='relu',
+                        padding='same')(x)
+
+x = Conv2DTranspose(filters=3,
+                    kernel_size=kernel_size,
+                    padding='same')(x)
+
+outputs = Activation('sigmoid', name='decoder_output')(x)
+
+# Instantiate Decoder Model
+decoder = Model(latent_inputs, outputs, name='decoder')
+decoder.summary()
+
+# Autoencoder = Encoder + Decoder
+# Instantiate Autoencoder Model
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+autoencoder.summary()
+
+autoencoder.compile(loss='mse', optimizer='adam')
+
+# Train the autoencoder
+autoencoder.fit(x_adv_train,
+                x_train,
+                validation_data=(x_adv_test, x_test),
+                epochs=10,
+                batch_size=batch_size)
+
+autoencoder.save('cifar_baseline.h5')
+# Predict the Autoencoder output from corrupted test images
+
