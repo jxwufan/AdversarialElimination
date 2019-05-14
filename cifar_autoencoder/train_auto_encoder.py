9ff927a3e808b5ba068cf4b867ef4167bbd635a9
Fan Wu <jxwufan@gmail.com>
2019-05-14 20:33:02 +0000

    cifar autoencoder

--- cifar_autoencoder/train_auto_encoder.py
+++ cifar_autoencoder/train_auto_encoder.py
@@ -18,22 +18,7 @@ import pickle
 
 num_classes = 10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-input_shape = x_train.shape[1:]
 
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
-
-x_train_mean = np.mean(x_train, axis=0)
-#TODO add mean
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-sess = tf.Session()
-keras.backend.set_session(sess)
-
-filepath = "./resnet56/cifar10_ResNet38v1_model.206.h5"
-model = load_model(filepath)
-
-scores = model.evaluate(x_test, y_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+print(x_train.shape)
