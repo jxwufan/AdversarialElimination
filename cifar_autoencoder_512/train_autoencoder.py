32554f26dcacfd60589397687f43f4e3be7a7f97
jxwufan <jxwufan@node.jxwufan-qv51217.kvmarm-pg0.wisc.cloudlab.us>
2019-05-16 14:11:37 -0500

    udpate

---
+++ cifar_autoencoder_512/train_autoencoder.py
@@ -0,0 +1,116 @@
+from __future__ import print_function
+import keras
+import tensorflow as tf
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
+from keras.models import load_model
+from keras.datasets import cifar10
+import numpy as np
+from keras.layers import Activation, Dense, Input
+from keras.layers import Conv2D, Flatten
+from keras.layers import Reshape, Conv2DTranspose
+from keras.models import Model
+from keras import backend as K
+from keras.datasets import mnist
+import os
+import pickle
+
+num_classes = 10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+image_size = 32
+# Network parameters
+input_shape = (image_size, image_size, 3)
+batch_size = 512
+kernel_size = 3
+latent_dim = 512
+# Encoder/Decoder number of CNN layers and filters per layer
+layer_filters = [32, 64]
+
+# Build the Autoencoder Model
+# First build the Encoder Model
+inputs = Input(shape=input_shape, name='encoder_input')
+x = inputs
+# Stack of Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use MaxPooling2D as alternative to strides>1
+# - faster but not as good as strides>1
+for filters in layer_filters:
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=2,
+               activation='relu',
+               padding='same')(x)
+
+# Shape info needed to build Decoder Model
+shape = K.int_shape(x)
+
+# Generate the latent vector
+x = Flatten()(x)
+latent = Dense(latent_dim, name='latent_vector')(x)
+
+# Instantiate Encoder Model
+encoder = Model(inputs, latent, name='encoder')
+encoder.summary()
+
+# Build the Decoder Model
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+# Stack of Transposed Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use UpSampling2D as alternative to strides>1
+# - faster but not as good as strides>1
+for filters in layer_filters[::-1]:
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=kernel_size,
+                        strides=2,
+                        activation='relu',
+                        padding='same')(x)
+
+x = Conv2DTranspose(filters=3,
+                    kernel_size=kernel_size,
+                    padding='same')(x)
+
+outputs = Activation('sigmoid', name='decoder_output')(x)
+
+# Instantiate Decoder Model
+decoder = Model(latent_inputs, outputs, name='decoder')
+decoder.summary()
+
+# Autoencoder = Encoder + Decoder
+# Instantiate Autoencoder Model
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+autoencoder.summary()
+
+autoencoder.compile(loss='mse', optimizer='adam')
+
+fg_train, fg_test = pickle.load(open("/data/cifar/fg.pkl"))
+
+input_data = np.concatenate([fg_train, x_train], axis=0)
+output_data = np.concatenate([x_train, x_train], axis=0)
+
+validation_input_data = np.concatenate([fg_test, x_test], axis=0)
+validation_output_data = np.concatenate([x_test, x_test], axis=0)
+
+# Train the autoencoder
+autoencoder.fit(input_data,
+                output_data,
+                validation_data=(validation_input_data, validation_output_data),
+                epochs=200,
+                batch_size=batch_size)
+
+autoencoder.save("autoencoder_fg.h5")
